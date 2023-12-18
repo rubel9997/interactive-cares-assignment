de01331acccce48db766d630eecccef41ef21d17
rubelmahamud <rubelmahamud9997@gmail.com>
2023-12-18 18:57:55 +0600

    password reset done

---
+++ Assignment-10/url-shortener/app/Http/Controllers/Api/Auth/PasswordResetController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LinkEmailRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Mail\ResetPasswordLink;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+
+class PasswordResetController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+
+    public function sendResetLinkEmail(LinkEmailRequest $request)
+    {
+        $appUrl = env('APP_URL');
+        $frontendUrl = env('FRONTEND_URL');
+
+        $url = URL::temporarySignedRoute('password.reset', now()->addMinutes(30), ['email' => $request->email]);
+
+        $urlReplaced = str_replace($appUrl, $frontendUrl, $url);
+
+        Mail::to($request->email)->send(new ResetPasswordLink($urlReplaced));
+
+        return response()->json(['message' => 'Reset Password sent to mail']);
+    }
+
+
+    public function reset(ResetPasswordRequest $request)
+    {
+        $user = User::whereEmail($request->email)->first();
+
+        if(!$user){
+            return response()->json(['message'=>'User does not exists our records'],response::HTTP_UNAUTHORIZED);
+        }
+
+        $user->password =Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['message' => 'Password Reset Successfully.'],response::HTTP_OK);
+    }
+
+
+}
--- Assignment-10/url-shortener/routes/auth.php
+++ Assignment-10/url-shortener/routes/auth.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\PasswordResetController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', LoginController::class);
 Route::post('logout', LogoutController::class);
 Route::post('register', RegisterController::class);
+Route::post('password/email', [PasswordResetController::class,'sendResetLinkEmail'])->name('password.email');
+Route::post('password/reset', [PasswordResetController::class,'reset'])->middleware('signed')->name('password.reset');
